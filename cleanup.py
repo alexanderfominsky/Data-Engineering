195c7555180d0e66ee94eeb8c5d2525acb42c4d7
Александр Фоминский <sanyafominsky@gmail.com>
2025-04-14 01:16:58 +1000

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,1 +1,19 @@
+import os
+import time
 
+# Путь к директории с файлами
+directory = 'D:\Yeah Buddy\Pi\Farpost-Jupiter\data_slices\\'  # Замените на ваш путь
+
+def cleanup_old_files():
+    current_time = time.time()
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        # Проверка, является ли файл старше 5 минут
+        if os.path.isfile(file_path) and (current_time - os.path.getmtime(file_path)) > 300:
+            os.remove(file_path)
+            print(f"Удален файл: {file_path}")
+
+if __name__ == "__main__":
+    while True:
+        cleanup_old_files()
+        time.sleep(300)  # 5 минут
